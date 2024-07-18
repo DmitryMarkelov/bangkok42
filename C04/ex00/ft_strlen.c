6186eaa226b88a87ec36a037dc67222d0b36cbde
Dmitry Markelov <dmimarke@z1t3c1.42bangkok.com>
2024-07-18 21:04:09 +0700

    C04 for home

---
+++ C04/ex00/ft_strlen.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmimarke <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 18:10:12 by dmimarke          #+#    #+#             */
+/*   Updated: 2024/07/18 18:10:14 by dmimarke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_strlen(char *str);
+
+int ft_strlen(char *str)
+{
+    unsigned int    size;
+
+    size = 0;
+    while (*str != '\0')
+    {
+        ++size;
+        ++str;
+    }
+    return (size);
+}
+
+int main(void)
+{
+    printf("%d", ft_strlen("Hello world1"));
+    return (0);
+}
---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmimarke <dmimarke@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 18:20:28 by dmimarke          #+#    #+#             */
+/*   Updated: 2024/07/18 19:51:34 by dmimarke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void c(int nb);
+
+void ft_putnbr(int nb)
+{
+    char    c;
+	
+    while (nb > 1)
+	{
+		if (nb < 10 && nb > 1)
+		{
+            c = nb + 48;
+            write(1, &c, 1);
+            //printf("%d", nb);
+			//rnb *= 10;
+			//rnb += nb % 10;
+		}
+        else
+        {
+            c = nb * 10 + 48;
+            write(1, &c, 1);
+        }
+		nb = nb / 10;
+        ft_putnbr(nb);
+    }
+}
+
+int main(void)
+{
+    ft_putnbr(42);
+    return (0);
+}
