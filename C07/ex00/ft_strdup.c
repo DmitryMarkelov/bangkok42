01521a999b8dcd28b2d89472647856b7daa50abb
Дмитрий Маркелов <dmitry@Dmitry-MBP13.local>
2024-07-25 17:03:11 +0700

    additional

--- C07/ex00/ft_strdup.c
+++ C07/ex00/ft_strdup.c
@@ -1,56 +1,34 @@
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-void	ft_putchar(char c)	
+char	*ft_strdup(char *src);;
+
+char	*ft_strdup(char *src)
 {
-	write(1, &c, 1);
-}
+	int	size;		
+	char	*dest;		
 
-void	ft_putstr(char *str)			/* Функция печати строки */
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	write(1, str, i);
-}
-
-int		ft_strlen(char *str)
-{
-	int l;
-
-	l = 0;
-	while (str[l])
-		l++;
-	return (l);
-}
-
-
-char	*ft_strdup(char *src)	
-{
-	int		l;		
-	char	*dest;			
-	l = ft_strlen(src);				
-	if ((dest = malloc(sizeof(*src) * (l + 1))) == ((void *)0))
-		return (((void *)0));		
-	while (l >= 0)					
-		dest[l] = src[l];			
-		l--;						
-	return (dest);					
-}
-
-
-int     main(int argc, char **argv)	
-{
-	char *dup;
-	
-	if (argc > 1)				
-	{
-		dup = ft_strdup(argv[1]);
-		ft_putstr(dup);
-		ft_putchar('\n');
-		free(dup);
+	size = 0;
+	while (src[size] != '\0')
+		++size;
+	if (size)
+	{	
+		size = 0;
+		dest = (char *)malloc(size * sizeof(char));
+		while (src[size] != '\0')
+		{
+			dest[size] = src[size];
+			++size;
+		}
 	}
+	else
+		return (NULL);
+	return (dest);				
+}
+
+
+int     main(void)	
+{
+	printf("%s", ft_strdup("HERE WE ARE"));
 	return (0);				
 }
