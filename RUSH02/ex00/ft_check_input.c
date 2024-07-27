4587c1a2abfdd2220f6e82f3436c2d406567b214
Dmitry Markelov <dmimarke@z2t3c2.42bangkok.com>
2024-07-27 16:59:34 +0700

    RUSH02

---
+++ RUSH02/ex00/ft_check_input.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_input.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmimarke <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/27 16:02:37 by dmimarke          #+#    #+#             */
+/*   Updated: 2024/07/27 16:11:07 by dmimarke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int     ft_is_dictionary(int argc, char **argv);
+int     ft_is_correct_input();
+
+int	ft_is_dictionary(int argc, char **argv)
+{
+
+	return (0);
+}
+
+int	ft_is_correct_input()
+{
+	return (0);
+}
